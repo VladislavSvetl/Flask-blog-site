7e4d64e9ec15a628cdc0837cf3d847b1e46a2161
svetl <v.svetlichniy1999@gmail.com>
2022-12-18 12:48:25 +0300

    Created templates

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return 'About us'
+@app.route('/works')
+def works():
+    return render_template('works.html')
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return 'User information: ' + name + ' ' + str(id)
 
 
 if __name__ == '__main__':
