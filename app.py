f997a30a4db8d2fd7c71804f64fc1181dfbc4613
svetl <v.svetlichniy1999@gmail.com>
2022-12-18 14:42:35 +0300

    Created Database and page 'create.html'

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+app.app_context().push()
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r' %self.id
 
 
 @app.route('/')
@@ -22,9 +37,9 @@ def blog():
     return render_template('blog.html')
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'User information: ' + name + ' ' + str(id)
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
